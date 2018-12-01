6902c49faede929b6faf97718668397b111a7115
nishantshah57 <nds280494@gmail.com>
2018-12-01 17:06:29 -0500

    made changes in kinematics

---
+++ mp_project/build/manipulation_and_grasping/CMakeFiles/tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorial.dir/tutorial.cpp.o"
+  "/home/nishant/Motion_Planning_Project/mp_project/devel/lib/manipulation_and_grasping/tutorial.pdb"
+  "/home/nishant/Motion_Planning_Project/mp_project/devel/lib/manipulation_and_grasping/tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mp_project/build/world_launch/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nishant/Motion_Planning_Project/mp_project/src/world_launch
+# Build directory: /home/nishant/Motion_Planning_Project/mp_project/build/world_launch
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
