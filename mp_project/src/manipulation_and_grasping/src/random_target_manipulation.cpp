061ccf1a96c26d36824349b682996ce75e3844bf
nishantshah57 <nds280494@gmail.com>
2018-12-02 10:57:14 -0500

    Random target manipulation node added

---
+++ mp_project/src/manipulation_and_grasping/src/random_target_manipulation.cpp
@@ -0,0 +1,20 @@
+// MoveIt Header file
+#include <moveit/move_group_interface/move_group.h>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "manipulation_node",
+  ros::init_options::AnonymousName);
+  // start a ROS spinning thread
+  // Always start spinner if using moveit
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+  // this connects to a running instance of the move_group node
+  // Here the Planning group is "left_arm"
+  move_group_interface::MoveGroup left_arm_group("left_arm");
+  // specify that our target will be a random one
+  left_arm_group.setRandomTarget();
+  // plan the motion and then move the group to the sampled target
+  left_arm_group.move();
+  ros::waitForShutdown();
+}
